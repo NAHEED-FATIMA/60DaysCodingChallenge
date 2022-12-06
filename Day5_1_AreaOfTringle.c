ef86221bb03fbbda0122b912f7fdc6d2e6dad661
NAHEED-FATIMA <119695135+NAHEED-FATIMA@users.noreply.github.com>
2022-12-06 20:47:01 +0530

    Add files via upload

---
+++ Day5_1_AreaOfTringle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main()
+    {
+    	float b,h,area;
+    	b=7;
+    	h=11;
+    	area = (b*h) / 2;
+    	printf("\n area of tringle is:%f",area);
+    	return 0;
+    	
+	}
---
+++ Day5_2_AreaOfSquare.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=15;  
+   int area_square=a*a;  
+   printf("Area of the square=%d",area_square);  
+} 
