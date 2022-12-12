30d85f38c5c5ca2ab19b2c963946aa06e2613b7a
NAHEED-FATIMA <119695135+NAHEED-FATIMA@users.noreply.github.com>
2022-12-12 18:58:03 +0530

    Add files via upload

---
+++ Day_11_1_AreaOfClinder.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  
+int main()   
+  {   
+    float r,  h, area;  
+    r=4.0;   
+    h = 8.0;  
+    area = (2*22*(r + h))/7;  
+    printf("\n\n Area of Cylinder is : %f", area);  
+    return 0;  
+   }  
